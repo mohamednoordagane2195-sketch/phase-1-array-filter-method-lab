6468146b39ef7a298fd086f850e2f106f8a80a7b
Mohamed Noor <mohamednoordagane2195@gmail.com>
2026-01-21 11:09:02 +0300

    welcome

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+// findMatching 
+// returns an array of drivers that match the passed in name
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// fuzzyMatch
+// returns all drivers whose names begin with the provided letters
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// matchName
+// returns all drivers that match the passed in name
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+// matchName
+// returns all drivers that match the passed in name
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+
+
